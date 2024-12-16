b8705dbcc4fba7e5e082da26af54306c09072e79
Miko <miko@gmail.com>
2024-12-16 14:05:59 +0500

    main page design 80%

--- coffee-shop/components/EditScreenInfo.tsx
+++ coffee-shop/components/EditScreenInfo.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, ImageBackground, Button, TouchableOpacity } from 'react-native';
 import { Text, View, } from './Themed';
 import * as Font from 'expo-font';
 export default function EditScreenInfo({ path }: { path: string }) {
@@ -8,7 +8,8 @@ export default function EditScreenInfo({ path }: { path: string }) {
   useEffect(() => {
     async function loadFonts() {
       await Font.loadAsync({
-        'Sora-Regular': require('../assets/fonts/Sora-VariableFont_wght.ttf'),
+        'Sora-Regular': require('../assets/fonts/Sora/Sora-Regular.ttf'),
+        'Sora-SemiBold': require('../assets/fonts/Sora/Sora-SemiBold.ttf'),
       });
       setFontsLoaded(true);
     }
@@ -19,14 +20,22 @@ export default function EditScreenInfo({ path }: { path: string }) {
     return null; // Показать загрузку или пустой экран
   }
   return (
-    <View style={styles.container}>
-      <Image
-        source={require('../assets/images/Main-Page_Banner.png')} // путь к изображению
-        style={{ height:500, marginTop:20}}
-        resizeMode="cover"
-      />
-      <Text style={styles.text}>Fall in Love with {'\n'} Coffee in Blissful {'\n'} Delight!</Text>
-    </View>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.imageBackground}
+          imageStyle = {styles.imageStyle}
+          source={require('../assets/images/Main-Page_Banner.png')}
+          >
+          
+          <View style={styles.blackBlock}>
+            <Text style={styles.text}>Fall in Love with {'\n'} Coffee in Blissful {'\n'} Delight!</Text>
+            <Text style={styles.subText}>Welcome to our cozy coffee corner, where {'\n'} every cup is a delightful for you.</Text>
+            <TouchableOpacity style={styles.button} onPress={() => alert('Button pressed!')}>
+              <Text style={styles.btnText}>Get Started</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </View>
   );
 }
 
@@ -36,13 +45,51 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start', // центрировать по вертикали
     alignItems: 'center', // центрировать по горизонтали
     backgroundColor: '#000', // цвет фона
-    width:'100%'
+    width:'100%',
   },
   text: {
-    fontFamily: 'Sora-Regular',
+    fontFamily: 'Sora-SemiBold',
     fontSize: 32,
-    fontWeight: '400',
     marginTop: 10,
+    color:'#fff',
     textAlign: 'center',
   },
+  subText:{
+    fontFamily: 'Sora-Regular',
+    fontSize: 14,
+    fontWeight: '500',
+    marginTop: 10,
+    color:'#A2A2A2',
+    textAlign: 'center',
+  },
+  imageBackground: {
+    width: '100%', // Задаем ширину 100% экрана
+    height: '100%', // Высота фона
+    justifyContent: 'flex-end', // Центрируем контент по вертикали
+    alignItems: 'center', // Центрируем контент по горизонтали
+  },
+  blackBlock: {
+    width:'100%',
+    height:360,
+    backgroundColor: '#0004', // Черный фон
+    alignItems:'center'
+  },
+  imageStyle: {
+    marginTop:20,
+    height:536
+  },
+  button:{
+    borderRadius:16,
+    paddingTop:16,
+    paddingBottom:16,
+    marginTop:32,
+    backgroundColor:'#C67C4E',
+    width:'80%',
+    alignItems:'center'
+  },
+  btnText:{
+    fontFamily: 'Sora-Regular',
+    fontSize: 16,
+    color:'#fff',
+  }
 });
